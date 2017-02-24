d930ec1d0f22a089b1d0d9504c7c0b9c89cf3d31
Alexsandro Lopes <Alexsandro Lopes@Alexsandro-PC>
2017-02-24 19:18:26 -0300

    9.2. Configurando o projeto

---
+++ brewer/src/main/java/com/aplopes/brewer/controller/CidadesController.java
@@ -0,0 +1,15 @@
+package com.aplopes.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CidadesController {
+	
+	@RequestMapping("/cidades/novo")
+	public String novo() {
+		return "cidade/CadastroCidade";
+	}
+	
+	
+}
